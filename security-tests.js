27c2b51e44972ca86cdf803690697ad121eef444
Ghettozero1 <147770041+Realdeli2@users.noreply.github.com>
2026-09-23 09:45:12 +0300

    Update security-tests.js

--- security-tests.js
+++ security-tests.js
@@ -16,14 +16,25 @@ window.verifyOTPCode = function() {
   const log = document.getElementById('mfaLog');
   const userCode = document.getElementById('otpInput')?.value.trim();
 
+  // Kod üretilmediyse veya zaten kullanıldıysa
   if (!generatedOTP) {
-    log.innerHTML = `<span style="color:#ff4d4d;">[HATA] Önce 'OTP Kodu Gönder' butonuna basmalısınız!</span>`;
+    log.innerHTML = `<span style="color:#ff4d4d;">[HATA] Önce 'OTP Kodu Gönder' butonuna basmalı veya yeni kod istemelisiniz!</span>`;
     return;
   }
 
+  // Doğrulama kontrolü
   if (userCode === generatedOTP) {
-    log.innerHTML += `<br><span style="color:#5cb85c; font-weight:bold;">[BAŞARILI 200 OK]</span> 2FA Doğrulandı! Oturum açıldı.`;
+    log.innerHTML = `
+      <span style="color:#5cb85c; font-weight:bold;">[BAŞARILI 200 OK]</span> 2FA Doğrulandı! Oturum açıldı.<br>
+      <small style="color:#aaa;">(OTP Kodu kullanıldı ve sıfırlandı)</small>
+    `;
+    // Güvenlik için OTP tek kullanımlık yapılır ve input temizlenir
+    generatedOTP = null; 
+    document.getElementById('otpInput').value = '';
   } else {
-    log.innerHTML += `<br><span style="color:#d9534f; font-weight:bold;">[REDDEDİLDİ 401]</span> Hatalı OTP Kodu! Erişim engellendi.`;
+    log.innerHTML = `
+      [SMS / AUTHENTICATOR] OTP Kodu: <b style="color:#00ff00;">${generatedOTP}</b><br>
+      <span style="color:#d9534f; font-weight:bold;">[REDDEDİLDİ 401]</span> Hatalı OTP Kodu! Erişim engellendi.
+    `;
   }
 };
